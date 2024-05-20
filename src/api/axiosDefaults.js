108c67160b8df11ef2687989488714cfe7e39058
ramonabroberg <ramme96@gmail.com>
2024-05-20 20:52:03 +0000

    Comment out console.log as errors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "https://musicmaniac-drf-api-961711dd9bd4.herokuapp.com/"
+axios.defaults.baseURL = "https://musicmaniac-drf-api-961711dd9bd4.herokuapp.com"
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
